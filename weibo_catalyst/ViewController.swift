70ac3bde9a9807a6b71c75506b5c9722d8e5f469
huluobo <hujewelz@163.com>
2020-06-28 11:09:34 +0800

    Init project

---
+++ weibo_catalyst/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weibo_catalyst
+//
+//  Created by huluobo on 2020/6/28.
+//  Copyright © 2020 huluobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
